fb56e120165c4d0b978ae5559294144ba3e5fd3f
Michel.P <michelpr1224@gmail.com>
2024-01-04 22:53:10 -0300

    style: apply background in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Footer.css';
 
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-    crossOrigin="anonymous"
-  />;
-
 function Footer() {
   return (
     <Navbar
